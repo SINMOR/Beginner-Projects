dad70d84bdf44f7a792a52fe45c9c590455fa7e6
Sindet <morrisindet@gmail.com>
2024-06-25 16:12:52 +0300

    word replacement

---
+++ wordreplacement/wordreplacement.py
@@ -0,0 +1,7 @@
+def replace():
+    str='Good Morning my name is John '
+    words_to_replace = input('Enter the word to replace:')
+    word_replacement= input('Enter the word replacement')
+    print(str.replace(words_to_replace,word_replacement))
+
+replace()
